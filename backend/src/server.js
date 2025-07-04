32f5eb4f288c7953dfb74ff81afd4dcc2bd1da69
Ashutosh Gupta <ashutoshgupta2737@gmail.com>
2025-07-05 02:20:50 +0530

    made changes for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,32 +1,45 @@
-import express from "express"
-import dotenv from "dotenv"
-import { connectDB } from "./config/db.js"
-import rateLimiter from "./middleware/rateLimiter.js"
-import cors from "cors"
-import notesRoutes from "./routes/notes.routes.js"
-import authRoutes from "./routes/auth.routes.js"
-import cookieParser from "cookie-parser"
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors";
+import notesRoutes from "./routes/notes.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
+import path from "path";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const PORT = process.env.PORT || 5001
+const app = express();
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
+// middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    })
+  );
+}
+app.use(express.json()); // this middleware parses json bodies: req.body
+app.use(rateLimiter);
+app.use(cookieParser());
 
-// middleware 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
-app.use(express.json()) // this middleware parses json bodies: req.body
-app.use(rateLimiter)
-app.use(cookieParser())
+app.use("/api/auth", authRoutes);
+app.use("/api/notes", notesRoutes);
 
-app.use("/api/auth", authRoutes)
-app.use("/api/notes", notesRoutes)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log("Server started on PORT: ", PORT)
-    })
-}) 
+  app.listen(PORT, () => {
+    console.log("Server started on PORT: ", PORT);
+  });
+});
